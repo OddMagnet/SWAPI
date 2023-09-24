d89095a0ef80869c2ded066d59119bc539863f4f
MICHAEL BRUENEN <mibruenen@gmail.com>
2023-09-24 17:07:22 +0200

    Initial Commit

---
+++ SWAPI/SWAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWAPIApp.swift
+//  SWAPI
+//
+//  Created by Michael Brünen on 24.09.23.
+//
+
+import SwiftUI
+
+@main
+struct SWAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
